0ea1007e83027d8c113ef11c6167e6d134215af4
Peter Antoncich <PeterA@nvisionit.co.za>
2023-03-04 10:18:07 +0200

    added exception handling for jar capacity

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,7 @@
 using Application.Interfaces;
 using Application.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,37 +20,57 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler(configure: webApp => webApp.Run(async ctx =>
+    {
+        await Results.Problem(
+                         ctx.Features
+                            .Get<IExceptionHandlerFeature>()
+                            ?.Error.Message)
+                     .ExecuteAsync(ctx);
+    })
+);
+
 app.UseHttpsRedirection();
 
-var simpleImplementation = app.MapGroup("").WithTags("simple");
+var simpleImplementation = app.MapGroup("")
+                              .WithTags("simple");
 
 simpleImplementation.MapGet("/total",
-    async (IAsyncCoinJar coinJar) => await coinJar.GetTotalAmountAsync());
+                        async (IAsyncCoinJar coinJar) => await coinJar.GetTotalAmountAsync()
+                    )
+                    .WithDescription("Gets the total value from the jar");
 
 simpleImplementation.MapPost("/add",
-    async (IAsyncCoinJar coinJar, Coin coin) =>
-        await coinJar.AddCoinAsync(coin)
-);
+                        async (IAsyncCoinJar coinJar, Coin coin) =>
+                            await coinJar.AddCoinAsync(coin)
+                    )
+                    .WithDescription("Adds a coin to the Jar (if possible)");
 
 simpleImplementation.MapPut("/reset",
-    async (IAsyncCoinJar coinJar) =>
-        await coinJar.ResetAsync()
-);
+                        async (IAsyncCoinJar coinJar) =>
+                            await coinJar.ResetAsync()
+                    )
+                    .WithDescription("Empties the jar");
 
-var restfulImplementation = app.MapGroup("/jar").WithTags("restful");
+var restfulImplementation = app.MapGroup("/jar")
+                               .WithTags("restful-ish");
 
 restfulImplementation.MapGet("",
-    async (IAsyncCoinJar coinJar) =>
-        await coinJar.GetTotalAmountAsync()
-);
+                         async (IAsyncCoinJar coinJar) =>
+                             await coinJar.GetTotalAmountAsync()
+                     )
+                     .WithDescription("Gets the total value from the jar");
 
 restfulImplementation.MapPost("",
-    async (IAsyncCoinJar coinJar, Coin coin) =>
-        await coinJar.AddCoinAsync(coin)
-);
+                         async (IAsyncCoinJar coinJar, Coin coin) =>
+                             await coinJar.AddCoinAsync(coin)
+                     )
+                     .WithDescription("Adds a coin to the Jar (if possible)");
 restfulImplementation.MapDelete("",
-    async (IAsyncCoinJar coinJar) =>
-        await coinJar.ResetAsync()
-);
+                         async (IAsyncCoinJar coinJar) =>
+                             await coinJar.ResetAsync()
+                     )
+                     .WithDescription(
+                         "Empties the jar - No real restful way to 'Reset' so mapped to delete");
 
 app.Run();
